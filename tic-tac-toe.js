23d9a3a98c9a8d3f57c117273a56c1b8d128e2cd
Kaylyn Duncan <kaylynduncan@unr.edu>
2025-06-13 23:24:26 -0700

    added makeX functionality and clear button works

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,3 +3,51 @@ Date: June 12, 2025
 Author: Kaylyn Duncan
 */
 
+//Game Playing Logic
+
+
+//Game Presentation Logic
+function toggleBtn(){
+  var btn = document.getElementById("btn");
+  var one = document.getElementById("one");
+  var two = document.getElementById("two");
+  var three = document.getElementById("three");
+  var four = document.getElementById("four");
+  var five = document.getElementById("five");
+  var six = document.getElementById("six");
+  var seven = document.getElementById("seven");
+  var eight = document.getElementById("eight");
+  var nine = document.getElementById("nine");
+  if(btn.value == "Clear"){
+    btn.value = "Start";
+    one.value = "";
+    one.disabled = false;
+    two.value = "";
+    two.disabled = false;
+    three.value = "";
+    three.disabled = false;
+    four.value = "";
+    four.disabled = false;
+    five.value = "";
+    five.disabled = false;
+    six.value = "";
+    six.disabled = false;
+    seven.value = "";
+    seven.disabled = false;
+    eight.value = "";
+    eight.disabled = false;
+    nine.value = "";
+    nine.disabled = false;
+  }
+  else {
+    
+  }
+}
+
+function makeX(num){
+  var btn = document.getElementById(num);
+  var start = document.getElementById("btn");
+  btn.value = "X";
+  btn.disabled = true;
+  start.value = "Clear";
+}
